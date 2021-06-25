9744700d188bbcda6a417298568f2ff8d40dcbb9
ban-mazin <ban.qaddoori89@gmail.com>
2021-06-25 14:20:00 -0600

    add usestate useefect hooks

--- chatapp/src/hooks/useLocalStorage.js
+++ chatapp/src/hooks/useLocalStorage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-export default function usLocalStorage() {
-  return (
-    <div>
-      
-    </div>
-  )
+const PREFIX = 'chatApp-clone-';
+
+export default function useLocalStorage(key, initialValue) {
+  const perfixedKey = PREFIX + key;
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(perfixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue)
+    if(typeof initialValue === 'function') {
+      return initialValue()
+    } else {
+      return initialValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(perfixedKey, JSON.stringify(value))
+  }, [perfixedKey, value])
+
+  return [value, setValue]
 }
