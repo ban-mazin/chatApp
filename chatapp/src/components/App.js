26d71e094d0f0695108ba98be3278e82e9a5d131
ban-mazin <ban.qaddoori89@gmail.com>
2021-07-15 10:17:30 -0600

    fix bug

--- chatapp/src/components/App.js
+++ chatapp/src/components/App.js
@@ -3,7 +3,7 @@ import Login from './Login';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
-const [id, setId] = useLocalStorage()
+const [id, setId] = useLocalStorage('id')
 
 
   return (
