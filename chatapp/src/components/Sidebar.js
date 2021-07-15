75afb197957cbf70c2446aa726597fce0f5b8e20
ban-mazin <ban.qaddoori89@gmail.com>
2021-07-15 12:19:30 -0600

    add style to sidebar

--- chatapp/src/components/Sidebar.js
+++ chatapp/src/components/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Tab, Nav } from 'react-bootstrap'
 
 export default function Sidebar({ id }) {
   return (
-    <div>
-      ban
+    <div style= {{ width: '250px'}} className="d-flex flex-column">
+      <Tab.Container>
+        <Nav>
+          <Nav.Item>
+            <Nav.Link>
+              Conversations
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+      </Tab.Container>
     </div>
   )
 }
